6fb2c9dafcf5818fca344b3e2da922fc73b33b0b
Ana Carolina de Barros <a.debarros@queensu.ca>
2025-01-12 12:00:57 -0500

    Added del_recipe function.

--- resippy.py
+++ resippy.py
@@ -1,5 +1,11 @@
 # resippy
 
+# Next steps:
+## Add delete function
+## Update new_recipe so that it can't add a recipe that's already in the table
+## Update update_menu so that it can't update a recipe that doesn't already exist in the table
+## Finish unittesting for Version 1
+
 import sqlite3
 import argparse
 import atexit
@@ -21,6 +27,11 @@ def new_recipe(args, **kwargs):
     Arguments:
         args(dict): Contains required argument --new and potential optional arguments --drumlin_rating, --lina_rating, --ian_rating, and --last_made.
     
+    Raises:
+        KeyError if the recipe name is missing
+        AssertionError if the last_made date is not formatted correctly
+        Database Error if something is wrong with the database
+
     Returns:
         If added properly, returns True and an empty string.
         If not added properly, returns False and the error.
@@ -68,6 +79,10 @@ def update_menu(args, **kwargs):
 
     Args:
         args (dict): Contains required argument --update_menu and potential optional arguments --drumlin_rating, --lina_rating, --ian_rating, and --last_made.
+    
+    Returns:
+        True and an empty string if the recipe is updated correctly.
+        False and the error if the last_made date is incorrect.
     """
     # Get Current Recipe Values
     cursor.execute("SELECT id FROM menu WHERE name=?", (args['update_menu'],))
@@ -92,6 +107,31 @@ def update_menu(args, **kwargs):
     query = "UPDATE menu SET {updates} WHERE {condition}".format(updates=updates_query, condition=condition_query)
 
     # Add the new recipe to the database
+    cursor.execute(query)
+    connection.commit()
+    return True, ''
+
+def delete_recipe(args, **kwargs):
+    """Deletes a recipe from the menu.
+
+    Args:
+        args (dict): Contains required argument --del_recipe and potential optional arguments --drumlin_rating, --lina_rating, --ian_rating, and --last_made.
+        
+    Returns:
+        True and an empty string if the recipe is deleted
+        False and an error string if the recipe was not found in the menu
+    """
+    # Get Recipe ID
+    cursor.execute("SELECT id FROM menu WHERE name=?", (args['del_recipe'],))
+    recipe_id = cursor.fetchall()
+    if len(recipe_id) == 1:
+        recipe_id = recipe_id[0][0]
+        id_query = "id=" + str(recipe_id)
+    elif len(recipe_id) == 0:
+        return False, "{} was not found in the menu. Please try again.".format(args['del_recipe'])
+   
+    # Delete Query
+    query = "DELETE FROM menu WHERE {id}".format(id=id_query)
     cursor.execute(query)
     connection.commit()
     return True, ''
@@ -129,6 +169,7 @@ def create_parser():
     parser.add_argument('--last_made', type=str, help="Date the recipe was last made (formatted as DD/MM/YYYY)")
     parser.add_argument('--viewmenu', action="store_true", help="View the menu.")
     parser.add_argument('--update_menu', help="Name of the recipe you would like to update in the menu")
+    parser.add_argument('--del_recipe', help="Name of the recipe you would like to delete from the menu")
     return parser
 
 def exit_handler():
@@ -150,11 +191,29 @@ if __name__ == "__main__":
             print("{} has been added to the homehold menu!".format(recipe_name))
         else:
             print("An error has ocurred. Please try again. \n Error Information: {error}".format(error=error))
+    ## View Menu
     if args.viewmenu:
         view_menu(vars(args))
+    ## Update Menu
     if args.update_menu:
         updated, error = update_menu(vars(args))
         if updated:
             print("{} has been updated in the homehold menu!".format(args.update_menu))
         else:
             print("An error has ocurred. Please try again. \n Error Information: {error}".format(error=error))
+    ## Delete a Recipe
+    if args.del_recipe:
+        confirm = input("{} will be permanently deleted from the homehold menu. Are you sure you would like to continue? [Y/N]  ".format(args.del_recipe))
+        deleted = False
+        while not deleted:
+            if confirm.strip().upper() == "Y":
+                deleted, error = delete_recipe(vars(args))
+                if deleted:
+                    print("{} has been deleted from the menu.".format(args.del_recipe))
+                else:
+                    print("An error has ocurred. Please try again. \n Error Information: {error}".format(error=error))
+                    deleted = True
+            elif confirm.strip().upper() == "N":
+                deleted = True
+            else:
+                confirm = input("Unrecognized argument. Please enter Y or N. \n {} will be permanently deleted from the homehold menu. Are you sure you would like to continue? [Y/N]  ".format(args.del_recipe))
